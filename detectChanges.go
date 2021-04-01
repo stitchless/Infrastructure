bbf752a81bca2bc8970df5286da138d3543a50b4
Josh Peizer <github@sent.sh>
2021-04-01 17:07:27 -0400

    test

--- detectChanges.go
+++ detectChanges.go
@@ -3,16 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	if len(os.Args) > 1 {
-		directories := make(map[int]string)
+		directories := make(map[string]string)
+		var output []string
+
 		for i, arg := range os.Args {
 			if i > 0 {
-				directories[i] = arg
+				directory := filepath.Dir(arg)
+				if _, ok := directories[directory]; !ok {
+					directories[directory] = directory
+					output = append(output, directory)
+				}
 			}
+			fmt.Println(output)
 		}
-		fmt.Println(directories)
 	}
 }

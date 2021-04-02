748d8b067fa06189bf7245fa037a92a0b57e6324
Josh Peizer <github@sent.sh>
2021-04-02 15:10:48 -0400

    sef

--- detectChanges.go
+++ detectChanges.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-	"path/filepath"
-	"strings"
 )
-
+// test string: ["Deployments/adguard/010-Namespace.yaml","Deployments/hello-world/010-Namespace.yaml","Deployments/hello-world/010-Namespace.yaml"]
 func main() {
 	if len(os.Args) > 1 {
-		directories := make(map[string]string)
+		//directories := make(map[string]string)
 		var changedDirectories []string
 
-		for i, arg := range os.Args {
-			if i > 0 {
-				directory := filepath.Dir(arg)
-				if _, ok := directories[directory]; !ok {
-					directories[directory] = directory
-					changedDirectories = append(changedDirectories, directory)
-				}
-			}
+		err := json.Unmarshal([]byte(os.Args[1]), &changedDirectories)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Println(changedDirectories)
+
+		//for i, arg := range os.Args {
+		//	if i > 0 {
+		//		directory := filepath.Dir(arg)
+		//		if _, ok := directories[directory]; !ok {
+		//			fmt.Println("Found No Entry")
+		//			directories[directory] = directory
+		//			changedDirectories = append(changedDirectories, directory)
+		//		}
+		//		fmt.Println("Found Entry for: " + directory)
+		//	}
+		//}
 		// Format: ["values","values"]
-		output := `["` + strings.Join(changedDirectories, `","`) + `"]`
-		fmt.Println(output)
+		//output := `['` + strings.Join(changedDirectories, `','`) + `']`
+		fmt.Println(changedDirectories)
 	}
 }

cd78226a2eeebf4bfebba13a13e532c327e6254f
Josh Peizer <github@sent.sh>
2021-04-02 14:25:18 -0400

    sefsxdg

--- detectChanges.go
+++ detectChanges.go
@@ -4,33 +4,25 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) > 1 {
-		//app := "echo"
 		directories := make(map[string]string)
-		var output []string
+		var changedDirectories []string
 
 		for i, arg := range os.Args {
 			if i > 0 {
 				directory := filepath.Dir(arg)
 				if _, ok := directories[directory]; !ok {
 					directories[directory] = directory
-					output = append(output, directory)
+					changedDirectories = append(changedDirectories, directory)
 				}
 			}
 		}
+		//fmt.Println(changedDirectories)
+		output := `["` + strings.Join(changedDirectories, `","`) + `]"`
 		fmt.Println(output)
-		// Send the results to the outputs of the github script step via echo command
-		//formattedArg := fmt.Sprintf("::set-output name=changed_output::%+v", output)
-		//fmt.Println(formattedArg)
-		//cmd := exec.Command(app, formattedArg)
-		//stdout, err := cmd.Output()
-		//
-		//if err != nil {
-		//	println(err.Error())
-		//}
-		//print(string(stdout))
 	}
 }

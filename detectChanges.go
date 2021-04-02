fb9f29ecd511453884d2dbe220f4d424b8842efb
Josh Peizer <github@sent.sh>
2021-04-02 17:31:04 -0400

    testing

--- detectChanges.go
+++ detectChanges.go
@@ -3,34 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
-// test string: ["Deployments/adguard/010-Namespace.yaml","Deployments/hello-world/010-Namespace.yaml","Deployments/hello-world/010-Namespace.yaml"]
+
+// test string: ["Deployments/adguard/020-Issuer.yaml","Deployments/hello-world/010-Namespace.yaml"]
 func main() {
 	if len(os.Args) > 1 {
-		//directories := make(map[string]string)
-		var changedDirectories []string
+		var arr []string
+		var uniqueDirectories []string
+		
+		dataJson := os.Args[1]
+		directories := make(map[string]string)
 
-		err := json.Unmarshal([]byte(os.Args[1]), &changedDirectories)
-		if err != nil {
-			log.Fatal(err)
+		_ = json.Unmarshal([]byte(dataJson), &arr)
+
+		for _, path := range arr {
+			directory := filepath.Dir(path)
+			if _, ok := directories[directory]; !ok {
+				fmt.Println("Found No Entry")
+				directories[directory] = directory
+				uniqueDirectories = append(uniqueDirectories, directory)
+			}
 		}
-		fmt.Println(changedDirectories)
-
-		//for i, arg := range os.Args {
-		//	if i > 0 {
-		//		directory := filepath.Dir(arg)
-		//		if _, ok := directories[directory]; !ok {
-		//			fmt.Println("Found No Entry")
-		//			directories[directory] = directory
-		//			changedDirectories = append(changedDirectories, directory)
-		//		}
-		//		fmt.Println("Found Entry for: " + directory)
-		//	}
-		//}
 		// Format: ["values","values"]
-		//output := `['` + strings.Join(changedDirectories, `','`) + `']`
-		fmt.Println(changedDirectories)
+		output := `['` + strings.Join(uniqueDirectories, `','`) + `']`
+		fmt.Printf("%v", output)
 	}
 }
